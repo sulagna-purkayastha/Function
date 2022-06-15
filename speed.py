afcc02b4527b62593791c2b844cc30be4998e255
sulagna-purkayastha <106422210+sulagna-purkayastha@users.noreply.github.com>
2022-06-15 11:44:29 +0530

    Update speed.py

--- speed.py
+++ speed.py
@@ -1,27 +1,16 @@
 # Q27. Write a function for checking the speed of drivers. This function
 # should have one parameter: speed.
 
-
-
-
 def my_func(speed):
     point=0
-    if speed<70:
-        return "ok"
-    elif speed>=70:
-        i=1
-        count=0
-        point=0
-        while i<=speed:
-            count=count+1
-            if count==5:
-                point= point+1
-                return "point =",point
-            i=i+1
-    if point>12:
-            return "License suspended" 
+    if speed>70:
+        speed=speed-70
+        div=speed//5
+        if div>12:
+            return "license suspended"
+        else:
+            return "points=",div
+    else:
+        return "ok" 
 speed=int(input("enter speed: "))
-print(my_func(speed))   
-
-        
-        
+print(my_func(speed))  
